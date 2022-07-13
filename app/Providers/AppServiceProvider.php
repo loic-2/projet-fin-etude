21a9b1f82a7811bae99e06cb385fc6018cc1273e
Ganno Loic <loicganno@gmail.com>
2022-07-13 08:52:23 +0100

    Modification du fichier Appserviceprovider de http pour https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,11 +22,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+           URL::forceScheme('https');
         }
     }
 }
